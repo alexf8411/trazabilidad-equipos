57ea9f502797cbbfb00290dac841167598c96c7e
Alex Fonseca <alexf8411@users.noreply.github.com>
2026-02-10 10:38:58 -0500

    Activacion de envio del correo2

--- core/config_mail.php
+++ core/config_mail.php
@@ -3,6 +3,6 @@
 define('SMTP_HOST', 'smtp.office365.com');
 define('SMTP_PORT', 587);
 define('SMTP_USER', 'recursos.tecnologicos@lab.urosario.edu.co'); // Poner el correo real aquí
-define('SMTP_PASS', 'bKeDYq3uAuCdZJU6GKEL'); // Contraseña de la cuenta
+define('SMTP_PASS', 'vpvdpwyqzmjxncgf'); // Contraseña de la cuenta
 define('SMTP_FROM_NAME', 'URTRACK - Activos TI');
-?>
+?> 
