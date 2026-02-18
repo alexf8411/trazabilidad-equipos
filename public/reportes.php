f5de213e3f9abcd9df1d361f490d311412dad664
Alex Fonseca <alexf8411@users.noreply.github.com>
2026-02-18 16:31:56 -0500

    reparar bug de reportes 4

--- public/reportes.php
+++ public/reportes.php
@@ -598,6 +598,7 @@ $cache_age = isset($_SESSION[$cache_key_time]) ? (time() - $_SESSION[$cache_key_
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reportes - URTRACK</title>
